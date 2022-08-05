b9389a4a8cc27f9f1deb286baf27f766e3dbc10b
gabor <gaborspaits1@gmail.com>
2022-08-05 13:03:26 +0200

    refactoring erlang version

--- ik.erl
+++ ik.erl
@@ -1,22 +1,22 @@
 -module(ik).
--export([arraysElement/2, patternInArray/2, patternInArrayTimes/2, fst/1, snd/1]).
-
-fst({A, _}) -> A.
-
-snd({_, B}) -> B.
+-export([arraysElement/2, patternInArray/2, patternInArrayTimes/2]).
 
 patternInArrayTimes(T,P) -> 
-	PIA = patternInArray(T, P),
-	case fst(PIA) of
-	 true -> patternInArrayTimes(snd(PIA),P) + 1;
+	{FOUND, REM} = patternInArray(T, P),
+	case FOUND of
+	 true -> patternInArrayTimes(REM, P) + 1;
 	 false -> 0 end.
 
 patternInArray(T, []) -> {true, T};
-patternInArray(T, [X|XS]) -> case fst(patternInArray(snd(arraysElement(T, X)), XS)) of
-  true -> {fst(arraysElement(T, X)),  snd(patternInArray(snd(arraysElement(T, X)), XS))};
-  false -> {false, T} end.
+patternInArray(T, [X|XS]) ->
+	{INIT, WOIT} = arraysElement(T, X),
+	{FOUND, REM} = patternInArray(WOIT, XS),
+	case FOUND of
+	 true -> {INIT,  REM};
+	 false -> {false, T} end.
 
 arraysElement([], _) -> {false, []};
 arraysElement([X|XS], C) when X =:= C -> {true, XS};
-arraysElement([X|XS], C) -> {fst(arraysElement(XS, C)),[X] ++ snd(arraysElement(XS, C))}.
-
+arraysElement([X|XS], C) -> 
+	{FOUND, REM} = arraysElement(XS, C),
+	{FOUND, [X] ++ REM}.
